fd2eea51be07c8f6aa4ccedb928e144ad40a8402
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-03-23 16:42:04 -0600

    cambios en evaluador y EvalFuncion

--- src/EvalFuncion.java
+++ src/EvalFuncion.java
@@ -1,3 +1,14 @@
-public class EvalFuncion {
+import java.util.List;
+
+public class EvalFuncion<E> {
     
+    public void fEvaluar(List<E> instruccion){
+        
+
+        if(instruccion.contains("quote")) {
+            new Evaluador<E>().quote(instruccion);
+        }else if(instruccion.contains("setq")) {
+            new Evaluador<E>().setq(instruccion);
+        }
+}
 }
--- src/Evaluador.java
+++ src/Evaluador.java
@@ -1,22 +1,54 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Evaluador<E> {
     public String funcion = "";
     private HashMap<String, Object> var = new HashMap<>();
     private List<E> instrucciones;
 /**
- * Función Para evaluar Quote dentro de Lisp
+ * MetodoPara evaluar Quote dentro de Lisp
  * @param instrucciones Lista con las instrucciones de la funcion del método
  */
-public void Quote(List<E> instrucciones){
+
+public void quote(List<E> instrucciones){
     	String texto="";
     	for(int i=1;i<instrucciones.size();i++) {
     		
     		texto+=instrucciones.get(i)+" ";
     		System.out.println(texto);
     	}
+    }
+    /**
+     * Metodo para poder evaluar la funcion de setq dentro de LISP
+     * @param instrucciones Lista con las instrucciones de la funcion del método
+     */
+
+    public void setq(List<E> instrucciones) {
+    	Map<String, String> map = new HashMap<String, String>();
+    	//Si son varios datos
+    	if(instrucciones.get(2).toString().charAt(0)=='['){
+    		
+    		String[] var = instrucciones.get(2).toString().split(",");
+    		String contenido="";
+    		for(int i=1;i<var.length;i++) {
+    			//System.out.println(var[i]);
+    			contenido+=var[i];
+    		}
+    		map.put(instrucciones.get(1).toString(), contenido);
+    		System.out.println("La variable es: "+instrucciones.get(1).toString());
+    		System.out.println("valor "+"["+map.get(instrucciones.get(1).toString()));
+    		
+    	//Si solo es un dato
+    	}else {
+    		
+    		map.put(instrucciones.get(1).toString(), instrucciones.get(2).toString());
+    		
+    		System.out.println("La variable es: "+instrucciones.get(1).toString());
+    		System.out.println("valor "+map.get(instrucciones.get(1).toString()));
+    	}
+    	
     }
 }
 
