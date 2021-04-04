9fc94152fcdba44a9b0ff5340f416dc5e49957a7
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-04-04 16:18:49 -0600

    enviar a evaluador

--- src/EvalFuncion.java
+++ src/EvalFuncion.java
@@ -7,20 +7,20 @@ public class EvalFuncion<E> {
 
     public void fEvaluar(ArrayList<E> instruccion){
         List<E> list;
-            
+        
         if (instruccion.contains("-") || instruccion.contains("/") || instruccion.contains("*") || instruccion.contains("+")){
-            	
+
             Calculadora cal = new Calculadora();
             //Realizar lo de Calculadora
             //break;
 
         } else if (instruccion.contains("list")){
-            	
+
             list = new Evaluador<E>().list(instruccion.subList(1, instruccion.size()));
             System.out.println("Lista Creada: " +list);
-            
+
         } else if (instruccion.contains("equal")){
-            
+
             if( (new Evaluador<E>()).equals(instruccion.get(1), instruccion.get(2))){
                 System.out.print("Resultado: , " + instruccion.get(1) + " es igual que " + instruccion.get(2));
             }
@@ -29,16 +29,16 @@ public class EvalFuncion<E> {
             }
         }
             else if (instruccion.contains(">")){
-            	
+
                 if( (new Evaluador<E>()).Mayor(instruccion.get(1), instruccion.get(2))){
                     System.out.print("Resultado: , " + instruccion.get(1) + " es mayor que " + instruccion.get(2));
                 }
                 else{
                     System.out.print("Resultado: " + instruccion.get(1) + " No es mayor que " + instruccion.get(2));
                 }
-                
+
             } else if (instruccion.contains("<")){
-            	
+
                 if( (new Evaluador<E>()).Menor(instruccion.get(1), instruccion.get(2))){
                     System.out.print("Resultado: " + instruccion.get(1) + " es menor que " + instruccion.get(2));
                 }
