9968ab67b690708d9edd47c9e879bf0cf6f407f2
JuanDiegoAvila <60203575+JuanDiegoAvila@users.noreply.github.com>
2021-04-06 20:34:35 -0600

    Comentarios lector

--- src/Lector.java
+++ src/Lector.java
@@ -9,6 +9,11 @@ public class Lector {
     HashMap<String,String> var = new HashMap<String,String>();
     int parentesis;
 
+    /**
+     * Metodo de LISP
+     * @param path recibe la direccion del documento de texto que contiene la instruccion LISP.
+     * @return regresa la lista con todas las instrucciones.
+     */
     public ArrayList<Object> LISP(String path){
         String resultado = "";
         try {
@@ -21,12 +26,18 @@ public class Lector {
 
         }
 
+        //agregar espacios para poder separar las listas.
         resultado = resultado.replaceAll("\\(","\\ ( ");
         resultado = resultado.replaceAll("\\)","\\ ) ");
 
         return Read(resultado);
     }
 
+    /**
+     * Metodo de LISP
+     * @param charac recibe una lista con todos los caracteres de la instruccion.
+     * @return regresa la lista con todas las instrucciones.
+     */
     public ArrayList<Object> createList(List<String> charac){
 
         int parentesis = 0;
@@ -40,18 +51,18 @@ public class Lector {
                 //agregar todo lo anterior.
                 switch (temp){
                     case "(" -> {
-                        if(temporal.contains(" ")){
+                        if(temporal.contains(" ")){//si hay espacio debe ingresar las palabras individualmente.
                             ArrayList<String> separar = new ArrayList<String>(Arrays.asList(temporal.split(" ")));
                             lista.addAll(separar);
                         }else{
                             lista.add(temporal);
                         }
-                        temporal = "";
+                        temporal = "";//se reinicia temporal.
                         parentesis++;
                     }
                     case ")" ->{
                         expresionTemp = createList(Arrays.asList(temporal.split("")));
-                        if(!expresionTemp.isEmpty()){
+                        if(!expresionTemp.isEmpty()){//Si no esta vacio lo agrega.
                             lista.add(expresionTemp);
                             parentesis--;
                             temporal = "";
@@ -92,6 +103,11 @@ public class Lector {
         return expresion;
     }
 
+    /**
+     * Metodo de LISP
+     * @param LispExpresion recibe un String con todos las instrucciones separadas por espacio.
+     * @return regresa la lista con todas las instrucciones.
+     */
     public ArrayList<Object> Read(String LispExpresion){
         LispExpresion = LispExpresion.replaceAll("\\s{2,}", " ");
 
@@ -100,17 +116,17 @@ public class Lector {
         List<String> charac = Arrays.asList(sep);
 
         ArrayList<Object> expresion = createList(charac);
+        //revisar todos los elementos en la lista expresion y revisa si hay contenido vacio.
         for(Object z : expresion){
             if(z instanceof List){
                 String contenido = (String) ((List<?>) z).get(0);
                 List<String> cont = new ArrayList<String>(Arrays.asList(contenido.split(" ")));
 
                 if(!cont.isEmpty()){
-                    cont.remove(0);
+                    cont.remove(0);//Elimina un espacio
                 }
 
                 for(int i = 0; i<cont.size(); i++){
-
                     if (i == 0) {
                         ((List<Object>) z).set(i, (Object) cont.get(i));
                     } else {
@@ -120,7 +136,7 @@ public class Lector {
             }
         }
 
-
+        //revisa todos los parentesis, si es igual a 0 esta bien escrito.
         if(parentesis != 0){
             System.out.println("Hace falta un parentesis para completar la expresion.");
             return null;
